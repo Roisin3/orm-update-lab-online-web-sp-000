0753395e617c6fe1410ca05c456365fc6b10bacd
Michael Lynch <michael@michaelrlynch.com>
2019-11-02 19:34:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,9 +55,10 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
+    sql = <<-SQL SELECT * FROM students WHERE name = ? LIMIT 1 SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.frist
   end
 
   def update
